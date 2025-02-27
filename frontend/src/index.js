b6cc42eb6ca9f3cd85007ae3199016231e4efe88
Python Julia DataScience (dotipynb.com) <71810412+PythonJulia@users.noreply.github.com>
2025-02-27 17:32:50 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./index.css"; // Ensure this file exists or remove this line if it doesn’t
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
