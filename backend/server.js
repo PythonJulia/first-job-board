5a786f7d323e535e80eb8876f228b756c1c8fb76
Python Julia DataScience (dotipynb.com) <71810412+PythonJulia@users.noreply.github.com>
2025-02-28 10:28:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const jobRoutes = require("./routes/jobRoutes");
-const authRoutes = require("./routes/authRoutes");
-
-dotenv.config();
 const app = express();
+const cors = require("cors");
 
 app.use(cors());
-app.use(bodyParser.json());
-
-app.use("/api/jobs", jobRoutes);
-app.use("/api/auth", authRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Backend is running!");
+    res.send("Backend is running on Vercel!");
 });
+
+module.exports = app;
